4502553bd3d0463a80e57cf06c32d007104129cc
Ryan Hadfield <ryanhadfield@gmail.com>
2021-06-01 13:16:16 -0600

    second commit

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path")
 
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function (req, res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+  })
 } 
 
 
